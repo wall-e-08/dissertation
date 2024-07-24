4e2ed1901ff59377dc39d9d85b8134fd2a714976
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2024-07-24 16:03:41 +0100

    added missing scripts from utils

--- utils/url.py
+++ utils/url.py
@@ -1,3 +1,5 @@
+import re
+
 import pandas as pd
 
 
@@ -8,3 +10,10 @@ def generate_urls(start_date, end_date):
   urls = [(base_url_xray.format(date.strftime('%Y/%m/%d')), base_url_dayobs.format(date.strftime('%Y/%m/%d'))) for date
           in date_list]
   return urls
+
+def region_from_url(url):
+  match = re.search(r'(\d+)\.html', url)
+  if match:
+    return match.group(1)
+  else:
+    return None
